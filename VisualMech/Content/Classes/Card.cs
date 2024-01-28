8ad1293600bbea508e183fd8da1e416dc6d6e915
Eureka Code <batagapaul10@gmail.com>
2024-01-28 16:35:29 +0800

    added class for dynamic adding of mechanic cards added search button, not yet finished on function

---
+++ VisualMech/Content/Classes/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VisualMech.Content.Classes
+{
+    public class Card
+    {
+        public string ImageSource { get; set; }
+        public string ThumbSource { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+        public string ConnectedPageName { get; set; }
+
+        public string GetHtml()
+        {
+            return $@"
+            <li class=""align-content-center justify-content-center m-auto"">
+                <a href=""{ConnectedPageName}.aspx"" class=""card"">
+                    <img src=""{ImageSource}"" class=""card__image"" alt="""" />
+                    <div class=""card__overlay"">
+                        <div class=""card__header"">
+                            <svg class=""card__arc"" xmlns=""http://www.w3.org/2000/svg""></svg>
+                            <img class=""card__thumb"" src=""{ThumbSource}"" alt="""" />
+                            <div class=""card__header-text"">
+                                <h3 class=""card__title"">{Title}</h3>
+                                <span class=""card__status"">{Status}</span>
+                            </div>
+                        </div>
+                        <p class=""fw-bolder card__title_description"">Description:</p>
+                        <p class=""card__description"">{Description}</p>
+                    </div>
+                </a>
+            </li>";
+        }
+    }
+}
