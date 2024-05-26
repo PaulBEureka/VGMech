74d030bbe7d371119ebb745b194f0fe632be3883
Eureka Code <batagapaul10@gmail.com>
2024-05-26 14:53:16 +0800

    Fixed responsiveness for leaderboard contents

--- VisualMech/Classes/MyHub.cs
+++ VisualMech/Classes/MyHub.cs
@@ -84,10 +84,24 @@ namespace VisualMech
 
             using (MySqlConnection connection = new MySqlConnection(connectionString))
             {
-                string query = $@"SELECT *, user.username, avatar.avatar_path FROM game_record 
-                               INNER JOIN user ON game_record.user_id = user.user_id 
-                               INNER JOIN avatar ON user.user_id = avatar.user_id
-                               WHERE game_record.game_title = '{miniGameTitle}' ORDER BY game_record.game_score DESC LIMIT 5";
+                string query = $@"
+                    SELECT 
+                        game_record.*, 
+                        user.username, 
+                        avatar.avatar_path 
+                    FROM 
+                        game_record 
+                    INNER JOIN 
+                        user ON game_record.user_id = user.user_id 
+                    INNER JOIN 
+                        avatar ON user.user_id = avatar.user_id 
+                    WHERE 
+                        game_record.game_title = '{miniGameTitle}' 
+                    ORDER BY 
+                        game_record.game_score DESC, 
+                        game_record.time_finished ASC 
+                    LIMIT 5";
+
 
                 using (MySqlCommand command = new MySqlCommand(query, connection))
                 {
@@ -104,27 +118,28 @@ namespace VisualMech
                             string score = reader["game_score"].ToString();
                             string avatarPath = reader["avatar_path"].ToString();
 
-                            allLeaderboardsString += $@"<div class=""row mt-3"" >
+                            allLeaderboardsString += $@"
+                                                <div class=""row my-3"">
                                                     <div class=""col-md-1 d-grid"">
                                                         <p class=""fw-bolder text-white fs-3 my-3"">{rankingNum.ToString()}.</p>
                                                     </div>
-                                                    <div class=""col-md-1 image-container me-3 mb-2"">
+                                                    <div class=""col-md-1 image-container me-3 mb-2 custom-leaderboard-avatar rounded-circle"">
                                                         <img src=""{avatarPath}"" alt="""" class=""rounded-circle record_border"" width=""60"" height=""60"">
                                                     </div>
-                                                    <div class=""col-md-9 leaderboard_white_rec_round"">
-                                                            <div class=""row"">
-                                                                <div class = ""col my-3"">
-                                                                    <p class=""text-start""><strong>{username.ToUpper()}</strong></p>
-                                                                </div>
-                                                                <div class =""col my-3"">
-                                                                    <p class=""text-center""><strong>Score:</strong>{score} pts</p>
-                                                                </div>
-                                                                <div class =""col my-3"">
-                                                                    <p class=""text-end""><strong>Time finished:</strong>{time_finished}</p>
-                                                                </div>
+                                                    <div class=""col-md-9 leaderboard_white_rec_round gap-3"">
+                                                        
+                                                            <div class=""mt-3"">
+                                                                <p class="" text-start""><strong>Player: </strong> {username}</p>
+                                                            </div>
+                                                            <div class=""mt-3"">
+                                                                <p class="" text-end""><strong>Score:</strong> {score} pts</p>
+                                                            </div>
+                                                            <div class=""mt-3"">
+                                                                <p class="" text-end""><strong>Time finished:</strong> {time_finished}</p>
                                                             </div>
                                                     </div>
                                                 </div>
+
                                                 <div>
                                                     <hr / class=""text-white"">
                                                 </div>";
