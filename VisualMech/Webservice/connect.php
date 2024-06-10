99f4b44ec80029829216e1d8b1934562dfa3cb26
Eureka Code <batagapaul10@gmail.com>
2024-06-10 14:25:13 +0800

    Updated to the new domain

---
+++ VisualMech/Webservice/connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mysql8010.site4now.net";
+$username = "aa9c62_visualg";
+$password = "Hello12345!"; 
+$dbname = "db_aa9c62_visualg"; 
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
